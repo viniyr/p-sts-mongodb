d6454cd86317d51be0097c4f729f1203b99e37aa
viniyr <vinicius.yonezawa@gmail.com>
2021-08-03 01:53:03 -0300

    Finding Post by ID

---
+++ src/main/java/com/viniweb/wksmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.viniweb.wksmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.viniweb.wksmongo.domain.Post;
+import com.viniweb.wksmongo.repository.PostRepository;
+import com.viniweb.wksmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
