87ff9f25b35311826e7125fb818be416a98ecac8
viniyr <vinicius.yonezawa@gmail.com>
2021-08-03 00:41:24 -0300

    Finding user by ID

--- src/main/java/com/viniweb/wksmongo/services/UserService.java
+++ src/main/java/com/viniweb/wksmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.viniweb.wksmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.viniweb.wksmongo.domain.User;
 import com.viniweb.wksmongo.repository.UserRepository;
+import com.viniweb.wksmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll() { 
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
