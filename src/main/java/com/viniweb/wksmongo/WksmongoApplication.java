c16a18c6b0887152dba9030b8ea510c5c739dcd9
viniyr <vinicius.yonezawa@gmail.com>
2021-08-02 22:58:40 -0300

    Project created

---
+++ src/main/java/com/viniweb/wksmongo/WksmongoApplication.java
@@ -0,0 +1,13 @@
+package com.viniweb.wksmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WksmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WksmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniweb/wksmongo/WksmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniweb.wksmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WksmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
