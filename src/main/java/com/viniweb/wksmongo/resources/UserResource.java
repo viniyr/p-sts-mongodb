89015054859c292a25325028a41629aebad8c7c1
viniyr <vinicius.yonezawa@gmail.com>
2021-08-03 00:51:09 -0300

    User insert with POST

--- src/main/java/com/viniweb/wksmongo/resources/UserResource.java
+++ src/main/java/com/viniweb/wksmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.viniweb.wksmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.viniweb.wksmongo.domain.User;
 import com.viniweb.wksmongo.dto.UserDTO;
@@ -36,4 +39,11 @@ public class UserResource {
 		
 	}
 	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) { 
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
