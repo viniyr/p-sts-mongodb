f34014e552ebdd77e1700b42903fe699f0cb8ef6
viniyr <vinicius.yonezawa@gmail.com>
2021-08-03 10:49:17 -0300

    Query with more parameters

--- src/main/java/com/viniweb/wksmongo/resources/util/URL.java
+++ src/main/java/com/viniweb/wksmongo/resources/util/URL.java
@@ -2,10 +2,14 @@ package com.viniweb.wksmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
-	public static String decodeParam(String text) { 
+	public static String decodeParam(String text) {
 		try {
 			return URLDecoder.decode(text, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
@@ -13,4 +17,13 @@ public class URL {
 		}
 	}
 	
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
+		}		
+	}
 }
